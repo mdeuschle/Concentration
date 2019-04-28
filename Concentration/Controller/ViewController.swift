aa0869670c1d11d8509c3ab9a2767eceab952352
Matthew Deuschle <mdeuschle@gmail.com>
2019-04-28 12:13:52 -0700

    set up UI

---
+++ Concentration/Controller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Matt Deuschle on 4/28/19.
+//  Copyright © 2019 Matt Deuschle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var collectionView: UICollectionView!
+    @IBOutlet var winLoseLabel: UILabel!
+    @IBOutlet var flipCountLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var playAgainButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
